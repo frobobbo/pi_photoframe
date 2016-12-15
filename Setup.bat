4d56b071c3aedb433be151ae256514489b0d924c
frobobbo <johnsobd@gmail.com>
2016-12-15 10:14:31 -0500

    Add files via upload

---
+++ Setup.bat
@@ -0,0 +1,7 @@
+c:\python27\scripts\pip install --upgrade pip
+c:\python27\scripts\pip install gdata
+c:\python27\scripts\pip install httplib2
+c:\python27\scripts\pip install oauth2client
+c:\python27\scripts\pip install Pillow
+c:\python27\scripts\pip install python-instagram
+c:\python27\scripts\pip install tlslite
