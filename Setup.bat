8fb7a12722c9d543e664921ab526e4b011a1d496
frobobbo <johnsobd@gmail.com>
2018-12-12 15:51:54 -0500

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,7 +1,9 @@
-c:\python27\scripts\pip install --upgrade pip
-c:\python27\scripts\pip install gdata
-c:\python27\scripts\pip install httplib2
-c:\python27\scripts\pip install oauth2client
-c:\python27\scripts\pip install Pillow
-c:\python27\scripts\pip install python-instagram
-c:\python27\scripts\pip install tlslite
+pip install --upgrade pip
+pip install gdata
+pip install httplib2
+pip install oauth2client
+pip install Pillow
+pip install python-instagram
+pip install tlslite
+pip install configparser
+pip install google-api-python-client
