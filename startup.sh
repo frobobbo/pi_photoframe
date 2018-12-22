b3dc434e11f9572b3cb182ad1c4503db585d6bfb
frobobbo <johnsobd@gmail.com>
2018-12-22 16:40:21 -0500

    Update - added config update

--- startup.sh
+++ startup.sh
@@ -2,4 +2,13 @@
 # startup.sh
 # launch the photo fram python script
 
+# stop the bootscreen service
+
+# download the latest config file
+curl -X POST -F "frameID=1" https://auth.cstoneweb.com/getConfig.php -o ./PhotoFrame/config.ini
+
+#launch the python script to download the files
 sudo -u pi python /home/pi/PhotoFrame/launch.py
+
+#launch the fbi slideshow
+sudo fbi -T 1 -noverbose -a -t 60 -u ~/PhotoFrame/photos/*
