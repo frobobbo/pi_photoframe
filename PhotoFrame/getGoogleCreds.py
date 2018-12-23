0dd2a021bfba93c5823cff245a1581a471e92bf5
frobobbo <johnsobd@gmail.com>
2018-12-23 12:12:05 -0500

    Add files via upload

--- PhotoFrame/getGoogleCreds.py
+++ PhotoFrame/getGoogleCreds.py
@@ -16,8 +16,11 @@ import time
 import urllib
 import urllib2
 import webbrowser
+from pathlib import Path
 
 from datetime import datetime, timedelta
+#from urllib.request import urlopen
+
 
 ########################################################################
 #                                                                      #
@@ -45,6 +48,13 @@ datURL = "https://auth.cstoneweb.com/auths/"
 Config = configparser.ConfigParser()
 Config.read("config.ini")
 
+def checkFileDate(url):
+	req = urllib2.Request(url)
+	url_handle = urllib2.urlopen(req)
+	headers = url_handle.info()
+ 
+	last_modified = headers.getheader("Last-Modified")
+	print last_modified
 
 ########################################################################
 #                                                                      #
@@ -52,13 +62,18 @@ Config.read("config.ini")
 #  configuration fiels.                                                #
 #                                                                      #
 ########################################################################
-
 def DownloadAuthfromGoogle():
 
 	gUserCount = int(Config.get('GoogleUsers','Count'))
 	for x in range(gUserCount):
 		gUserID = Config.get('GoogleUser' + str(x+1),'userid')
-		urllib.urlretrieve(datURL+gUserID+'.dat',os.path.join(configdir,gUserID+'.dat'))
+		uDatUrl = datURL+gUserID+'.dat'
+		uDatFile = Path(os.path.join(configdir,gUserID+'.dat'))
+		if uDatFile.exists():
+			print "File Exists - do Nothing"
+		else:
+			print "File not found - Download"
+			urllib.urlretrieve(uDatUrl,os.path.join(configdir,gUserID+'.dat'))
 	
 #Download photos from Google Photos Album
 if Config.get('Plugins','GooglePhotos') == 'True':
