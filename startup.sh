84179caf19bddc09abc39a46e5074ff25bcb3ced
frobobbo <johnsobd@gmail.com>
2018-12-23 12:30:04 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,8 +2,6 @@
 # startup.sh
 # launch the photo fram python script
 
-# stop the bootscreen service
-
 # download the latest config file
 # TODO: This URL should and frameID should be pulled from a Global Config file
 curl -X POST -F "frameID=1" https://auth.cstoneweb.com/getConfig.php -o ./PhotoFrame/config.ini
@@ -13,6 +11,9 @@ sudo -u pi python /home/pi/PhotoFrame/getGoogleCreds.py
 
 #launch the python script to download the files
 sudo -u pi python /home/pi/PhotoFrame/launch.py
+
+# stop the bootscreen service
+sudo service bootscreen stop
 
 #launch the fbi slideshow
 sudo fbi -T 1 -noverbose -a -t 60 -u ~/PhotoFrame/photos/*
