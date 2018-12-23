d5e5312701dcffaa580f72ec555eb5c3504d32ce
frobobbo <johnsobd@gmail.com>
2018-12-23 12:14:49 -0500

    Update wake.sh

--- cronjobs/wake.sh
+++ cronjobs/wake.sh
@@ -4,11 +4,5 @@
 
 sudo /opt/vc/bin/tvservice -p; fbset -depth 8; fbset -depth 16
 
-# download the latest config file
-curl -X POST -F "frameID=1" https://auth.cstoneweb.com/getConfig.php -o ./PhotoFrame/config.ini
-
-#launch the python script to download the files
-sudo -u pi python /home/pi/PhotoFrame/launch.py
-
-#launch the fbi slideshow
-sudo fbi -T 1 -noverbose -a -t 60 -u ~/PhotoFrame/photos/*
+# execute startup
+sh /home/pi/startup.sh
